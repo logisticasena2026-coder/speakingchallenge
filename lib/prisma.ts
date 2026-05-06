8247d232189262f0d9b96c43bf086533150a4731
compraxio <luisangelacu10@gmail.com>
2026-05-06 10:03:17 -0500

    Ayudaaaaaa22

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from "../generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-const globalForPrisma = global as unknown as {
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient;
 };
 const adapter = new PrismaPg({
@@ -11,5 +11,5 @@ const prisma =
   new PrismaClient({
     adapter,
   });
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 export default prisma;
