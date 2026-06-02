ff63c3c6db43984248731f40724d200ef60b5bde
compraxio <luisangelacu10@gmail.com>
2026-06-01 20:58:53 -0500

    fix: add missing useEffect import in EstadisticasDeFrases

--- components/Practica/EstadisticasDeFrases.tsx
+++ components/Practica/EstadisticasDeFrases.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useFrasesStore } from '@/store/useFrasesStore';
 import { usePracticaStore } from '@/store/usePracticaStore';
